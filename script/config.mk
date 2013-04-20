1c69c85af6db7513c3d437ff35b130603fb3b068
Andreas Fitzek <andreas.fitzek@turix.at>
2013-04-20 09:14:46 +0200

    Added prebuild polarSSL
    Signed-off-by: Andreas Fitzek <andreas.fitzek@turix.at>

--- script/config.mk
+++ script/config.mk
@@ -12,8 +12,13 @@ HOST_PREFIX :=
 ARM_TOOLCHAIN_DIR := /home/andy/UNI/DiplArbeit/source/toolchain/bin/bin/
 ARM_PREFIX := arm-none-eabi-
 ARM_TOOLCHAIN_LIB_DIR := /home/andy/apps/CodeSourcery_ARM_EABI_2012/lib/gcc/arm-none-eabi/4.6.3/
+
 ARM_NEWLIB_LIB_DIR := $(ANDIX_PREBUILD_DIR)/newlib/lib
 ARM_NEWLIB_INC_DIR := $(ANDIX_PREBUILD_DIR)/newlib/include
+
+ARM_POLARSSL_LIB_DIR := $(ANDIX_PREBUILD_DIR)/polarSSL/lib
+ARM_POLARSSL_INC_DIR := $(ANDIX_PREBUILD_DIR)/polarSSL/include
+
 
 PERL := $(shell which perl)
 
