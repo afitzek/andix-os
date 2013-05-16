4c3b289e263c2b9893b861e6192984a2029f5422
Andreas Fitzek <andreas.fitzek@turix.at>
2013-05-16 08:42:20 +0200

    Tools upgrade

--- src/tools/scripts/tz_load.sh
+++ src/tools/scripts/tz_load.sh
@@ -4,5 +4,7 @@ echo "Loading Kernel module...\n"
 sh scripts/tz_tee.kld
 echo "Kernel module loaded\n"
 
+./norm/daemons/tzsd.bin &
+
 LD_LIBRARY_PATH=$PWD/norm/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
--- src/tools/scripts/tz_unload.sh
+++ src/tools/scripts/tz_unload.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+killall tzsd.bin
+
 echo "Unloading Kernel modules...\n"
 sh scripts/tz_tee.kuld
 echo "Kernel modules unloaded\n"
+
