b4b09c9d8a47aeaf5f2c72bb81f782bb13cb7d26
Andreas Fitzek <andreas.fitzek@turix.at>
2013-07-16 21:39:24 +0200

    QEMU NFS root. We need 3 toolchains

--- script/config.mk
+++ script/config.mk
@@ -20,11 +20,17 @@ ARM_NEWLIB_INC_DIR := $(ANDIX_PREBUILD_DIR)/newlib/include
 ARM_TROPICSSL_LIB_DIR := $(ANDIX_PREBUILD_DIR)/tropicssl/lib
 ARM_TROPICSSL_INC_DIR := $(ANDIX_PREBUILD_DIR)/tropicssl/include
 
-ARM_NORM_TOOLCHAIN_DIR := 
-ARM_NORM_PREFIX := arm-linux-gnueabi-
+#ARM_NORM_TOOLCHAIN_DIR := $(ANDIX_DIR)/toolchain_norm/bin/
+ARM_NORM_TOOLCHAIN_DIR :=
+#ARM_NORM_PREFIX := armel-linux-
+ARM_NORM_KERN_PREFIX := arm-linux-gnueabi-
+ARM_NORM_PREFIX := arm-linux-gnueabihf-
+#ARM_NORM_TOOLCHAIN_DIR := 
+#ARM_NORM_PREFIX := arm-linux-gnueabi-
 #ARM_NORM_TOOLCHAIN_LIB_DIR := /home/andy/apps/CodeSourcery_ARM_EABI_2012/lib/gcc/arm-none-eabi/4.6.3/
 
-LINUX_KERNEL_DIR := /home/andy/UNI/DiplArbeit/3rdParty/imx-devel/KERNEL
+#LINUX_KERNEL_DIR := /home/andy/UNI/DiplArbeit/3rdParty/imx-devel/KERNEL
+LINUX_KERNEL_DIR := /home/andy/UNI/DiplArbeit/linaro-kernel/linux-linaro-tracking
 
 PERL := $(shell which perl)
 
