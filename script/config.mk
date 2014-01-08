c5d013249711ceae34c4b88c62fdec27927f17e3
Florian Achleitner <florian.achleitner@student.tugraz.at>
2014-01-08 10:11:49 +0100

    Switch build to newlib2 and gcc 4.8.2.
    Disable clang and build everything with gcc. (Mono doesn't like clang too much).
    gcc is built from source.

--- script/config.mk
+++ script/config.mk
@@ -12,11 +12,12 @@ HOST_PREFIX :=
 ARM_TOOLCHAIN_DIR := $(ANDIX_DIR)/toolchain/bin/
 ARM_PREFIX := arm-none-eabi-
 #ARM_TOOLCHAIN_LIB_DIR := /home/andy/apps/CodeSourcery_ARM_EABI_2012/lib/gcc/arm-none-eabi/4.6.3/
-ARM_TOOLCHAIN_LIB_DIR := $(ANDIX_DIR)/prebuild/tz_libgcc/
+ARM_TOOLCHAIN_LIB_DIR := $(ANDIX_DIR)/toolchain/lib/gcc/arm-none-eabi/4.8.1/
+#ARM_TOOLCHAIN_LIB_DIR := $(ANDIX_DIR)/prebuild/tz_libgcc/
 #ARM_TOOLCHAIN_LIB_DIR := $(ANDIX_DIR)/prebuild/cs_libgcc/
 
-ARM_NEWLIB_LIB_DIR := $(ANDIX_PREBUILD_DIR)/newlib/lib
-ARM_NEWLIB_INC_DIR := $(ANDIX_PREBUILD_DIR)/newlib/include
+ARM_NEWLIB_LIB_DIR := $(ANDIX_PREBUILD_DIR)/newlib2/arm-none-eabi/lib
+ARM_NEWLIB_INC_DIR := $(ANDIX_PREBUILD_DIR)/newlib2/arm-none-eabi/include
 
 ARM_TROPICSSL_LIB_DIR := $(ANDIX_PREBUILD_DIR)/tropicssl/lib
 ARM_TROPICSSL_INC_DIR := $(ANDIX_PREBUILD_DIR)/tropicssl/include
@@ -31,6 +32,7 @@ ARM_NORM_PREFIX := arm-linux-gnueabihf-
 #ARM_NORM_TOOLCHAIN_LIB_DIR := /home/andy/apps/CodeSourcery_ARM_EABI_2012/lib/gcc/arm-none-eabi/4.6.3/
 
 LINUX_KERNEL_DIR := $(ANDIX_DIR)/../linux-linaro-tracking/
+#LINUX_KERNEL_DIR := $(ANDIX_DIR)/../imx-devel/KERNEL/
 ROOTFS_LIBDIR := $(ANDIX_DIR)/../linaro-userland/binary/lib/arm-linux-gnueabihf
 # QEMU KERNEL
 # QEMU ANDROID KERNEL 
